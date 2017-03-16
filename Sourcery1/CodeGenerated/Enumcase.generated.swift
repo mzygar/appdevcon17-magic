b3832c9030f7ddadf665e2b846e09367e9cfb561
Michal Zygar <m.zygar@catawiki.nl>
2017-03-16 09:55:26 +0100

    Project structure & Added enum count

---
+++ Sourcery1/CodeGenerated/Enumcase.generated.swift
@@ -0,0 +1,13 @@
+// Generated using Sourcery 0.5.8 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+
+extension Animals { 
+	static var count: Int { return 5 }
+
+}
+
+extension Sweets { 
+	static var count: Int { return 3 }
+
+}
---
+++ Sourcery1/Enums.swift
@@ -0,0 +1,26 @@
+//
+//  Enums.swift
+//  Sourcery1
+//
+//  Created by Michal Zygar on 16/03/2017.
+//  Copyright © 2017 Catawiki. All rights reserved.
+//
+
+import Foundation
+
+
+enum Animals {
+    case Pig
+    case Slime
+    case Dog
+    case Cat
+    case Monkey
+
+}
+
+
+enum Sweets {
+    case Icecream,
+    Cake,
+    Chocolate
+}
